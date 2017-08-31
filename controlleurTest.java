db4f46392495fe115b333b414fdc1263f68aa131
JoelleTannous <joelle.tannous@isae.edu.lb>
2017-08-31 23:53:07 +0300

    Add files via upload

---
+++ controlleurTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetaccov;
+
+/**
+ *
+ * @author user
+ */
+public class controlleurTest 
+{
+    public static void main(String[] args) 
+    {
+		controlleur controlleur = new controlleur("200");
+		controlleur.start();
+}
+}
